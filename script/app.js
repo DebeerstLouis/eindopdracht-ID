8275fd7ead9eba15dbb23304e28cc8390ddeadb9
debeerst louis <55881630+DebeerstLouis@users.noreply.github.com>
2020-11-19 10:57:48 +0100

    icons added

--- script/app.js
+++ script/app.js
@@ -1,51 +1,51 @@
 // --Hulpmiddels voor het omrekenen van gegevens--
 const calc_temp = temp => {
     celcius = temp - 273.15
-    result=Math.round(celcius*100)/100
+    result = Math.round(celcius * 10) / 10
     return result
 }
-const calc_farenheigt =  temp=> {
-    fahrenheit = (temp - 273.15)* 1.8000 + 32.00
-    result=Math.round(fahrenheit*100)/100
+const calc_farenheigt = temp => {
+    fahrenheit = (temp - 273.15) * 1.8000 + 32.00
+    result = Math.round(fahrenheit * 10) / 10
     return result
 }
-var calc_wind = function(deg){
-    if (deg>11.25 && deg<33.75){
-      return "NNE";
-    }else if (deg>33.75 && deg<56.25){
-      return "ENE";
-    }else if (deg>56.25 && deg<78.75){
-      return "E";
-    }else if (deg>78.75 && deg<101.25){
-      return "ESE";
-    }else if (deg>101.25 && deg<123.75){
-      return "ESE";
-    }else if (deg>123.75 && deg<146.25){
-      return "SE";
-    }else if (deg>146.25 && deg<168.75){
-      return "SSE";
-    }else if (deg>168.75 && deg<191.25){
-      return "S";
-    }else if (deg>191.25 && deg<213.75){
-      return "SSW";
-    }else if (deg>213.75 && deg<236.25){
-      return "SW";
-    }else if (deg>236.25 && deg<258.75){
-      return "WSW";
-    }else if (deg>258.75 && deg<281.25){
-      return "W";
-    }else if (deg>281.25 && deg<303.75){
-      return "WNW";
-    }else if (deg>303.75 && deg<326.25){
-      return "NW";
-    }else if (deg>326.25 && deg<348.75){
-      return "NNW";
-    }else{
-      return "N"; 
+var calc_wind = function(deg) {
+        if (deg > 11.25 && deg < 33.75) {
+            return "NNE";
+        } else if (deg > 33.75 && deg < 56.25) {
+            return "ENE";
+        } else if (deg > 56.25 && deg < 78.75) {
+            return "E";
+        } else if (deg > 78.75 && deg < 101.25) {
+            return "ESE";
+        } else if (deg > 101.25 && deg < 123.75) {
+            return "ESE";
+        } else if (deg > 123.75 && deg < 146.25) {
+            return "SE";
+        } else if (deg > 146.25 && deg < 168.75) {
+            return "SSE";
+        } else if (deg > 168.75 && deg < 191.25) {
+            return "S";
+        } else if (deg > 191.25 && deg < 213.75) {
+            return "SSW";
+        } else if (deg > 213.75 && deg < 236.25) {
+            return "SW";
+        } else if (deg > 236.25 && deg < 258.75) {
+            return "WSW";
+        } else if (deg > 258.75 && deg < 281.25) {
+            return "W";
+        } else if (deg > 281.25 && deg < 303.75) {
+            return "WNW";
+        } else if (deg > 303.75 && deg < 326.25) {
+            return "NW";
+        } else if (deg > 326.25 && deg < 348.75) {
+            return "NNW";
+        } else {
+            return "N";
+        }
     }
-  }
-// --
-// --Data dat wordt getoont--
+    // --
+    // --Data dat wordt getoont--
 const showData_celsius = data => {
     //console.log(data)
     temp = data.main.temp;
@@ -83,50 +83,57 @@ const showData_Wind_and_pressure = data => {
     winddir = data.wind.deg
     windname = calc_wind(winddir)
     windspeed_convert = windspeed * 3.6
-    result=Math.round(windspeed_convert*100)/100
-    //console.log(windspeed_convert)
+    result = Math.round(windspeed_convert * 100) / 100
+        //console.log(windspeed_convert)
     document.querySelector('.js-Windspeed').innerText = `${result} KM/u`;
     document.querySelector('.js-direction').innerText = `${windname}`;
     document.querySelector('.js-pressure').innerText = `${pressure} pha`
 }
 const showData_location_icon = data => {
-    Locatie = data.name;
-    //description = data.weather
-    document.querySelector('.js-locatie').innerText = `${Locatie}`;
-    //document.querySelector('.js-icondescription').innerText = `${description}`;
-    const d = new Date();
-    buddy =  d.toUTCString()
-    document.querySelector('.js-currentdata').innerText = `${buddy}`;
-}
-// --
-// --data ophalen ++ controleren op toggle switch ++ huidige locatie opvragen--
-const getAPI = async (position) => {
+        Locatie = data.name;
+        //description = data.weather
+        document.querySelector('.js-locatie').innerText = `${Locatie}`;
+        for (var i in data.weather) {
+            Weather = data.weather[i]
+            document.querySelector('.js-icondescription').innerText = `${Weather.main}`;
+            Icon_name = Weather.icon;
+            iconurl = `/icons/${Icon_name}.svg`
+
+            document.querySelector('.js-icon').innerHTML = `<img src="${iconurl}" alt="Wether icon">`;
+        }
+        const d = new Date();
+        buddy = d.toUTCString()
+        document.querySelector('.js-currentdata').innerText = `${buddy}`;
+    }
+    // --
+    // --data ophalen ++ controleren op toggle switch ++ huidige locatie opvragen--
+const getAPI = async(position) => {
     lat = position.coords.latitude
     lon = position.coords.longitude
     const data = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=4dc82a8db4261d0aaa945c76431fda04`)
-    .then((r) => r.json())
-    .catch((err) => console.error('Error: ', err))
+        .then((r) => r.json())
+        .catch((err) => console.error('Error: ', err))
     showData_Wind_and_pressure(data);
     showData_location_icon(data);
     showData_celsius(data);
     // controleren op de toggle switch voor celsius naar fahrenheit
     var checkbox = document.querySelector('input[type="checkbox"]');
-    checkbox.addEventListener('change', function () {
+    checkbox.addEventListener('change', function() {
         if (checkbox.checked) {
-        showData_farenheigt(data)
-        // console.log('Checked');
+            showData_farenheigt(data)
+                // console.log('Checked');
         } else {
-        showData_celsius(data);
-        // console.log('Not checked');
+            showData_celsius(data);
+            // console.log('Not checked');
         };
     });
 };
 
 function getLocation() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(getAPI);
-    } else { 
-      console.log("Geolocation is not supported by this browser.");
+        navigator.geolocation.getCurrentPosition(getAPI);
+    } else {
+        console.log("Geolocation is not supported by this browser.");
     }
 }
 // --
@@ -134,7 +141,7 @@ function getLocation() {
 
 
 
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
     console.log('Script ingeladen');
     getLocation();
     // getAPI(50.8868222, 3.4323622)
