7a61399b490af86d49c8f91d6f73eeb22777381c
debeerst louis <55881630+DebeerstLouis@users.noreply.github.com>
2020-12-08 09:49:51 +0100

    change

--- script/app.js
+++ script/app.js
@@ -1,4 +1,4 @@
-// --Hulpmiddels voor het omrekenen van gegevens--
+// --Hulpfunctie voor het omrekenen van gegevens--
 const calc_temp = temp => {
     celcius = temp - 273.15
     result = Math.round(celcius * 10) / 10
@@ -10,41 +10,47 @@ const calc_farenheigt = temp => {
     return result
 }
 var calc_wind = function(deg) {
-        if (deg > 11.25 && deg < 33.75) {
-            return "NNE";
-        } else if (deg > 33.75 && deg < 56.25) {
-            return "ENE";
-        } else if (deg > 56.25 && deg < 78.75) {
-            return "E";
-        } else if (deg > 78.75 && deg < 101.25) {
-            return "ESE";
-        } else if (deg > 101.25 && deg < 123.75) {
-            return "ESE";
-        } else if (deg > 123.75 && deg < 146.25) {
-            return "SE";
-        } else if (deg > 146.25 && deg < 168.75) {
-            return "SSE";
-        } else if (deg > 168.75 && deg < 191.25) {
-            return "S";
-        } else if (deg > 191.25 && deg < 213.75) {
-            return "SSW";
-        } else if (deg > 213.75 && deg < 236.25) {
-            return "SW";
-        } else if (deg > 236.25 && deg < 258.75) {
-            return "WSW";
-        } else if (deg > 258.75 && deg < 281.25) {
-            return "W";
-        } else if (deg > 281.25 && deg < 303.75) {
-            return "WNW";
-        } else if (deg > 303.75 && deg < 326.25) {
-            return "NW";
-        } else if (deg > 326.25 && deg < 348.75) {
-            return "NNW";
-        } else {
-            return "N";
-        }
+    if (deg > 11.25 && deg < 33.75) {
+        return "NNE";
+    } else if (deg > 33.75 && deg < 56.25) {
+        return "ENE";
+    } else if (deg > 56.25 && deg < 78.75) {
+        return "E";
+    } else if (deg > 78.75 && deg < 101.25) {
+        return "ESE";
+    } else if (deg > 101.25 && deg < 123.75) {
+        return "ESE";
+    } else if (deg > 123.75 && deg < 146.25) {
+        return "SE";
+    } else if (deg > 146.25 && deg < 168.75) {
+        return "SSE";
+    } else if (deg > 168.75 && deg < 191.25) {
+        return "S";
+    } else if (deg > 191.25 && deg < 213.75) {
+        return "SSW";
+    } else if (deg > 213.75 && deg < 236.25) {
+        return "SW";
+    } else if (deg > 236.25 && deg < 258.75) {
+        return "WSW";
+    } else if (deg > 258.75 && deg < 281.25) {
+        return "W";
+    } else if (deg > 281.25 && deg < 303.75) {
+        return "WNW";
+    } else if (deg > 303.75 && deg < 326.25) {
+        return "NW";
+    } else if (deg > 326.25 && deg < 348.75) {
+        return "NNW";
+    } else {
+        return "N";
     }
-    // --
+}
+
+// --
+// hulpfunctie voor sleep
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+// --
 
 // --Data dat wordt getoont--
 const showData_celsius = data => {
@@ -94,22 +100,23 @@ const showData_Wind_and_pressure = data => {
     document.querySelector('.js-pressure').innerText = `${pressure} pha`
 }
 const showData_location_icon = data => {
-        Locatie = data.name;
-        //description = data.weather
-        document.querySelector('.js-locatie').innerText = `${Locatie}`;
-        for (var i in data.weather) {
-            Weather = data.weather[i]
-            document.querySelector('.js-icondescription').innerText = `${Weather.main}`;
-            Icon_name = Weather.icon;
-            iconurl = `icons/${Icon_name}.svg`
+    Locatie = data.name;
+    //description = data.weather
+    document.querySelector('.js-locatie').innerText = `${Locatie}`;
+    for (var i in data.weather) {
+        Weather = data.weather[i]
+        document.querySelector('.js-icondescription').innerText = `${Weather.main}`;
+        Icon_name = Weather.icon;
+        iconurl = `icons/${Icon_name}.svg`
 
-            document.querySelector('.js-icon').innerHTML = `<img src="${iconurl}" alt="Wether icon">`;
-        }
-        const d = new Date();
-        buddy = d.toLocaleString()
-        document.querySelector('.js-currentdata').innerText = `${buddy}`;
+        document.querySelector('.js-icon').innerHTML = `<img src="${iconurl}" alt="Wether icon">`;
     }
-    // --
+    const d = new Date();
+    buddy = d.toLocaleString()
+    document.querySelector('.js-currentdata').innerText = `${buddy}`;
+}
+
+// --
 
 // --data ophalen ++ controleren op toggle switch ++ huidige locatie opvragen--
 const getAPI = async(position) => {
@@ -141,9 +148,7 @@ const getAPI = async(position) => {
     });
 };
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+
 
 function showZoom() {
     TextTemp.classList.add('vieuw-zoom');
